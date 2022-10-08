d8d71428802881617c50e70575c14413e7f37080
Daniyal Alam <90774564+DaniyalAlam09@users.noreply.github.com>
2022-10-08 16:53:07 +0500

    Forgot password Implemented

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 app.use(express.json());
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+var nodemailer = require("nodemailer");
 const cors = require("cors");
 app.use(cors());
-
 const jwt = require("jsonwebtoken");
+var nodemailer = require("nodemailer");
+
 const JWT_SECRET =
   "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
 
@@ -70,21 +73,91 @@ app.post("/login", async (req, res) => {
     res.json({ status: "error", error: "InvAlid Password" });
   });
 
-// app.post("/post", async(req, res)=>{
-//     console.log(req.body);
-//     const {name} = req.body;
 
-//     try {
-//         if(name == "Daniyal"){
-//             res.send({status:"ok"})
-//         }
-//         else{
-//             res.send({status:"user not found"})
-//         }
-
-//     } catch (error) {
-//         res.send({status:"error"})
-
-//     }
-
-// })
+  app.post("/forgot-password", async (req, res) => {
+    const { email } = req.body;
+    try {
+      const oldUser = await customer.findOne({ email });
+      if (!oldUser) {
+        return res.json({ status: "User Not Exists!!" });
+      }
+      const secret = JWT_SECRET + oldUser.password;
+      const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+        expiresIn: "5m",
+      });
+      const link = `http://localhost:5000/reset-password/${oldUser._id}/${token}`;
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "dannyalalam09@gmail.com",
+          pass: "fvmultatpzjvmeqm",
+        },
+      });
+  
+      var mailOptions = {
+        from: "youremail@gmail.com",
+        to: email,
+        subject: "Password Reset",
+        text: link,
+      };
+  
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+          res.json({ status: `Email Sent to ${email} for verification` });
+        }
+      });
+      console.log(link);
+    } catch (error) {}
+  });
+  
+  app.get("/reset-password/:id/:token", async (req, res) => {
+    const { id, token } = req.params;
+    console.log(req.params);
+    const oldUser = await customer.findOne({ _id: id });
+    if (!oldUser) {
+      return res.json({ status: "User Not Exists!!" });
+    }
+    const secret = JWT_SECRET + oldUser.password;
+    try {
+      const verify = jwt.verify(token, secret);
+      return res.render("index", { email: verify.email, status: "Not Verified" });
+    } catch (error) {
+      console.log(error);
+      res.send("Not Verified");
+    }
+     res.send("done");
+  });
+  
+  app.post("/reset-password/:id/:token", async (req, res) => {
+    const { id, token } = req.params;
+    const { password } = req.body;
+  
+    const oldUser = await customer.findOne({ _id: id });
+    if (!oldUser) {
+      return res.json({ status: "User Not Exists!!" });
+    }
+    const secret = JWT_SECRET + oldUser.password;
+    try {
+      const verify = jwt.verify(token, secret);
+      const encryptedPassword = await bcrypt.hash(password, 10);
+      await customer.updateOne(
+        {
+          _id: id,
+        },
+        {
+          $set: {
+            password: encryptedPassword,
+          },
+        }
+      );
+      
+  
+      res.render("index", { email: verify.email, status: "verified" });
+    } catch (error) {
+      console.log(error);
+      res.json({ status: "Something Went Wrong" });
+    }
+  });
